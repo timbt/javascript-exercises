8f9bf9377c33799887855a1534d927e425cec47c
Tim Beattie <trbeattie@outlook.com>
2019-03-05 21:10:43 -0500

    Complete exercise removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
 const removeFromArray = function() {
-
+    const args = Array.prototype.slice.call(arguments);
+    const array = args[0];
+    const elementsToRemove = args.slice(1);
+    return array.filter(element => !(elementsToRemove.includes(element)));
 }
 
 module.exports = removeFromArray
