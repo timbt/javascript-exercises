2a2bd3d1288d17a449f230a007038592a97ee3fc
Tim Beattie <trbeattie@outlook.com>
2019-03-05 21:23:06 -0500

    Complete Exercise sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0){
+        return 'ERROR';
+    }
+    let sum = 0;
+    let smallerNumber = a < b ? a : b;
+    let largerNumber = a > b ? a : b;
+    for(let i = smallerNumber; i <= largerNumber; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
