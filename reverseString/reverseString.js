41cb4382f9e64e1e2f93aad58b997b251c4a6f85
Tim Beattie <trbeattie@outlook.com>
2019-03-05 20:16:15 -0500

    Complete exercise reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = '';
+    for (let char of string) {
+        newString = char + newString;
+    }
+    return newString;
 }
 
 module.exports = reverseString
