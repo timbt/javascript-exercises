19156ed1d3e46772ecb7463f94910801abe5e4f3
Tim Beattie <trbeattie@outlook.com>
2019-03-05 20:04:23 -0500

    Complete repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(baseString, repeats) {
+    let result = '';
+    try {
+        const range = [...Array(repeats).keys()];
+        for (let x in range) {
+            result += baseString;
+        }
+        return result;
+    } catch (e) {
+        return 'ERROR';
+    }
 }
 
-module.exports = repeatString
+module.exports = repeatString;
