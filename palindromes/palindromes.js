c19a1f4f9a54b617823e1b159805bb80cb2993ed
Tim Beattie <trbeattie@outlook.com>
2019-03-15 17:27:12 -0400

    Complete exercise palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
+const cleanString = string => string.replace(/[^0-9a-z]/gi, '').toLowerCase();
 
+const palindromes = function(originalString) {
+    const string = cleanString(originalString);
+    if (string.length < 2) {
+        return true;
+    } else if (string[0] === string[string.length - 1]) {
+        return palindromes(string.substring(1, string.length - 1));
+    }   else {
+        return false;
+    }
 }
 
 module.exports = palindromes
